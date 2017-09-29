aa6f02f1b4d9750b5526ff26492e75ebfaa5a0d6
jorditruji <jordimoreratrujillo@gmail.com>
2017-09-29 13:39:06 +0200

    data

--- Dataset_utils/write_dataset_names.py
+++ Dataset_utils/write_dataset_names.py
@@ -44,12 +44,13 @@ for folders in names:
 		#		path_to_file=path_to_folder+files
 		#		images.append(path_to_file)
 			if fileExtension == '.mat':
-				print (path_to_file)
-				print(path_to_jpg)
+
 				path_to_file=path_to_folder+fileName+'.mat'
 				path_to_jpg = path_to_folder+fileName[0:12]+'.color.jpg'
 				labels.append(path_to_file)
 				images.append(path_to_jpg)
+				print (path_to_file)
+				print(path_to_jpg)
 
 
 images = np.array(images)
