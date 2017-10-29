2a7d49abe37622f2c4046975b9ddaf89a0b4f3ec
jorditruji <jordimoreratrujillo@gmail.com>
2017-10-29 13:14:11 +0100

    neteja del dataset

--- Dataset_utils/delete_old_labels.py
+++ Dataset_utils/delete_old_labels.py
@@ -10,5 +10,5 @@ scannet_path ='/projects/world3d/2017-06-scannet/'
 
 folders = load_names_from_folders(scannet_path)
 for folder in folders:
-	cmd = 'find '+folder+' -name "*.mat"'
+	cmd = 'find '+scannet_path+folder+' -name "*.mat"'
 	subprocess.call(cmd, shell=True)
